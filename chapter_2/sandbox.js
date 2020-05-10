7ac9c344d1014b7d99af7f3c5776980a7c84f0a3
Shanna J Hullaby <52468970+Shannahullaby@users.noreply.github.com>
2020-05-10 15:07:02 -0400

    Update sandbox.js

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,1 +1,56 @@
-alert('hello, world');
+// // // let age =25;
+// // // let year= 2020;
+// // // console.log(age, year);
+// // // age =30;
+// // // console.log(age);
+// // // const points = 100;
+// // // console.log(points);
+
+// // // strings
+
+// // console.log('hello, world');
+
+// // let email = 'shanna.hullaby@gmail.com';
+
+// // console.log(email);
+
+// // let firstName ='Shanna';
+// // let lastName ='Hullaby';
+
+// // // let fullName = firstName +' '+ lastName;
+// // // console.log (fullName);
+// // // // getting characters 
+// // // console.log(fullName[0]);
+// // // //string length
+// // // console.log (fullName.length);
+// // // //String Methods
+// // // console.log(fullName.toUpperCase());
+// // // let result = fullName.toLocaleLowerCase();
+// // // console.log(result, fullName)
+// // // let index = email.indexOf('@');
+// // // console.log(index);
+
+// // //Common String Methods
+//  let email ='shanna@gmail.com';
+// //  let  result = email.lastIndexOf('n');
+// //  console.log (result);
+
+// //let result = email.replace('a','v');
+
+// console.log(result);
+// //slice
+let radius = 10;
+let pi = 3.14;
+// console.log(radius, pi);
+// console.log(10/2);
+// let result = radius % 3;
+// let result = pi*radius**2;
+//Order of operation PEMDAS (Parethesis exponents multiplication division addition Subtraction)
+// let result = 5* (10-3)**2;
+let likes = 10;
+// likes = likes + 1;
+// likes++;
+// likes +=10;
+// likes --;
+likes -=5;
+console.log(likes);
