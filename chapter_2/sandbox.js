8b392b0db848971f3c35c1a80fb6e94dba470764
Shanna J Hullaby <52468970+Shannahullaby@users.noreply.github.com>
2020-05-10 18:30:50 -0400

    Update sandbox.js

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,56 +1,136 @@
-// // // let age =25;
-// // // let year= 2020;
-// // // console.log(age, year);
-// // // age =30;
-// // // console.log(age);
-// // // const points = 100;
-// // // console.log(points);
+// // // // // let age =25;
+// // // // // let year= 2020;
+// // // // // console.log(age, year);
+// // // // // age =30;
+// // // // // console.log(age);
+// // // // // const points = 100;
+// // // // // console.log(points);
 
-// // // strings
+// // // // // strings
 
-// // console.log('hello, world');
+// // // // console.log('hello, world');
 
-// // let email = 'shanna.hullaby@gmail.com';
+// // // // let email = 'shanna.hullaby@gmail.com';
 
-// // console.log(email);
+// // // // console.log(email);
 
-// // let firstName ='Shanna';
-// // let lastName ='Hullaby';
+// // // // let firstName ='Shanna';
+// // // // let lastName ='Hullaby';
 
-// // // let fullName = firstName +' '+ lastName;
-// // // console.log (fullName);
-// // // // getting characters 
-// // // console.log(fullName[0]);
-// // // //string length
-// // // console.log (fullName.length);
-// // // //String Methods
-// // // console.log(fullName.toUpperCase());
-// // // let result = fullName.toLocaleLowerCase();
-// // // console.log(result, fullName)
-// // // let index = email.indexOf('@');
-// // // console.log(index);
+// // // // // let fullName = firstName +' '+ lastName;
+// // // // // console.log (fullName);
+// // // // // // getting characters 
+// // // // // console.log(fullName[0]);
+// // // // // //string length
+// // // // // console.log (fullName.length);
+// // // // // //String Methods
+// // // // // console.log(fullName.toUpperCase());
+// // // // // let result = fullName.toLocaleLowerCase();
+// // // // // console.log(result, fullName)
+// // // // // let index = email.indexOf('@');
+// // // // // console.log(index);
 
-// // //Common String Methods
-//  let email ='shanna@gmail.com';
-// //  let  result = email.lastIndexOf('n');
-// //  console.log (result);
+// // // // //Common String Methods
+// // //  let email ='shanna@gmail.com';
+// // // //  let  result = email.lastIndexOf('n');
+// // // //  console.log (result);
 
-// //let result = email.replace('a','v');
+// // // //let result = email.replace('a','v');
 
+// // // console.log(result);
+// // // //slice
+// // let radius = 10;
+// // let pi = 3.14;
+// // // console.log(radius, pi);
+// // // console.log(10/2);
+// // // let result = radius % 3;
+// // // let result = pi*radius**2;
+// // //Order of operation PEMDAS (Parethesis exponents multiplication division addition Subtraction)
+// // // let result = 5* (10-3)**2;
+// // let likes = 10;
+// // // likes = likes + 1;
+// // // likes++;
+// // // likes +=10;
+// // // likes --;
+// // likes -=5;
+// // console.log(likes);
+
+// // let results = 'the blog has ' + likes + ' likes';
+// // console.log (results);
+// // template strings
+// // const title ='Best reads of 2020';
+// // const author = 'Shanna';
+// // const likes = 30;
+// // //concatenation way
+// // // let results = 'the blog called ' + title + ' by ' + author + ' has ' + likes + ' likes'; 
+// // // console.log (results);
+
+// // //template string way (template literal)
+// // let result = `The blog called ${title} by ${author} has ${likes} likes`;
+// // console.log (result);
+
+
+// // //creating html templates
+// // let html = `
+// //     <h2>${title}</h2>
+// //     <p>${author}</p>
+// //     <span>This blog has ${likes}</span>
+// // `;
+// // console.log(html);
+
+// //Arrays
+// // let ninjas = ['shanna', 'travis','joe']; 
+// // // // ninjas[1]= 'ken';
+// // // // console.log(ninjas[1]);
+// // // let ages = [ 20, 25, 30, 35]; 
+// // // console.log(ages[2]);
+
+// // // let random = ['shanna', 'jarvis', 14.4, 7];
+// // // console.log (random);
+// // // console.log(ninjas.length);
+// // //array methods
+// // // let results = ninjas.join(',');
+// // // let results =ninjas.indexOf('shanna');
+// // // let results = ninjas.concat(['jarvis', 'crystal']);
+// // let results = ninjas.push('ken'); 
+// // results = ninjas.pop();
+// // console.log(results); 
+// let age = null;
+// console.log (age, age + 3, `the age is ${age}`);
+//let email = 'shanna@gmail.com';
+// let result = email.includes('@');
+// let names = ['mario','luigi','toad'];
+// let result= names.includes('luigi'); 
 // console.log(result);
-// //slice
-let radius = 10;
-let pi = 3.14;
-// console.log(radius, pi);
-// console.log(10/2);
-// let result = radius % 3;
-// let result = pi*radius**2;
-//Order of operation PEMDAS (Parethesis exponents multiplication division addition Subtraction)
-// let result = 5* (10-3)**2;
-let likes = 10;
-// likes = likes + 1;
-// likes++;
-// likes +=10;
-// likes --;
-likes -=5;
-console.log(likes);
+let age = 25;
+// console.log(age == 25);
+// console.log(age == 35);
+// console.log(age != 25);
+// console.log(age > 25);
+// console.log(age <= 25);
+// console.log(age >= 25);
+// console.log(age < 25);
+
+// let name = 'shanna';
+// console.log(name=='shanna');
+// console.log(name=='Shanna');
+// console.log(name>'crystal');
+// console.log(name>'Shanna');
+// console.log(name>'Crystal');
+
+// console.log(age == 25);
+// console.log(age == '25');
+// console.log(age != 25);
+// console.log (age != '25');
+// console.log (age === 25);
+// console.log(age === '25');
+// console.log(age !== 25);
+// console.log(age !== '25');
+let score = '100';
+// score = Number( score);
+// console.log(score + 1);
+// console.log (typeof score);
+// let result = Number('hello');
+// console.log(result);
+// let result = String(50);
+console.log(result, typeof result);
